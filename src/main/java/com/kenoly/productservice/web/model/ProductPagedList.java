b82f88bedc42ba82fe93851d674b5fb58629a513
AtumaKen <atumaken@gmail.comgit config --global user.name AtumaKengit commitgit statusgit statusgitgit config --global user.email atumaken@gmail.com>
2021-02-13 15:19:55 +0100

    add web data model.

---
+++ src/main/java/com/kenoly/productservice/web/model/ProductPagedList.java
@@ -0,0 +1,16 @@
+package com.kenoly.productservice.web.model;
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public class ProductPagedList extends PageImpl<ProductDto> {
+    public ProductPagedList(List<ProductDto> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public ProductPagedList(List<ProductDto> content) {
+        super(content);
+    }
+}
---
+++ src/main/java/com/kenoly/productservice/web/model/ProductStyleEnum.java
@@ -0,0 +1,5 @@
+package com.kenoly.productservice.web.model;
+
+public enum ProductStyleEnum {
+    TOYOTA, NISSAN, MAZDA, HONDA, SUBARU, INNOSON, PEUGOT
+}
