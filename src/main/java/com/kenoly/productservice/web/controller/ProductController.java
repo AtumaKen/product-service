a2c5bd62980b6737154d477de426b2358cd557b8
AtumaKen <atumaken@gmail.comgit config --global user.name AtumaKengit commitgit statusgit statusgitgit config --global user.email atumaken@gmail.com>
2021-02-13 15:35:37 +0100

    created controllers. Closes #1

---
+++ src/main/java/com/kenoly/productservice/web/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.kenoly.productservice.web.controller;
+
+import com.kenoly.productservice.web.model.ProductDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/product")
+@RestController
+public class ProductController {
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<ProductDto> getProductById(@PathVariable UUID productId){
+        //todo impl
+        return new ResponseEntity<>(ProductDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity saveNewProduct(@PathVariable ProductDto productDto){
+    //todo impl
+    return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity updateProductById(@PathVariable("productId") UUID productId, @RequestBody ProductDto productDto){
+        //todo impl
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
